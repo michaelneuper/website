13240654324d85a5fb2d9d867fed1debb05f62fd
Michael Neuper <michael@michaelneuper.com>
2023-10-30 18:26:36 +0200

    Add copy button to code blocks

---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+// Add copy button to <pre> blocks
+function addCopyButton() {
+    const codeBlocks = document.querySelectorAll('pre');
+    codeBlocks.forEach((codeBlock) => {
+        const copyButton = document.createElement('button');
+        copyButton.innerHTML = 'Copy';
+        copyButton.classList.add('copyButton');
+        copyButton.addEventListener('click', () => {
+            const textToCopy = codeBlock.innerText.trim();
+            copyToClipboard(textToCopy);
+        });
+        codeBlock.appendChild(copyButton);
+    });
+}
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            console.log('Text copied to clipboard:', text);
+        })
+        .catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+}
+
+addCopyButton();
